a965cb4b817c506886d46975ec8474399b41b657
PlavorSeol <pseol2190@outlook.com>
2019-05-26 04:30:12 +0900

    Update translations.php

--- translations.php
+++ translations.php
@@ -83,98 +83,122 @@ $translations = [
 	'mobile2text' => [
                 'en' => 'Our site is mobile optimised, so that all users are equal regardless of which device they use!',
                 'es' => 'Nuestro sitio está optimizado para dispositivos móviles, por lo que todos los usuarios son iguales, independientemente del dispositivo que utilicen.',
+		"ko"=>"우리의 사이트는 모바일 최적화, 그래서 모든 사용자들은 어떤 디바이스를 사용하던간에 평등합니다!"
         ],
 	'faqtext' => [
                 'en' => 'Frequently Asked Questions',
                 'es' => 'Preguntas frecuentes',
+		"ko"=>"자주 묻는 질문"
         ],
 	 'wikiqtext' => [
                 'en' => 'Which Wiki software do you run?',
                 'es' => '¿Qué software de Wiki ejecutas?',
+		 "ko"=>"어떤 위키 소프트웨어를 돌립니까?"
         ],
 	'wikiatext' => [
                 'en' => 'We run <a href="https://mediawiki.org">MediaWiki</a>, the same software that powers <a href="https://wikipedia.org">Wikipedia</a>.',
                 'es' => 'Ejecutamos <a href="https://mediawiki.org"> MediaWiki </a>, el mismo software que impulsa <a href="https://wikipedia.org"> Wikipedia </a>.',
+		"ko"=>"우리는 <a href='https://mediawiki.org'>미디어위키</a>를 사용하며, <a href='https://wikipedia.org'>위키백과</a>를 돌리는 것과 같습니다."
         ],
 	'sslqtext' => [
                 'en' => 'Are you secure?',
                 'es' => '¿Estás seguro?',
+		"ko"=>"당신은 안전합니까?"
         ],
 	'sslatext' => [
                 'en' => 'Yes we are, we recently disabled TLS 1.0 and TLS 1.1 in favour of being TLS 1.2+, you can read this blog post <a href="https://phabricator.miraheze.org/phame/post/view/4/potentially_breaking_security_changes/">here</a>.',
                 'es' => 'Sí, recientemente inhabilitamos TLS 1.0 y TLS 1.1 a favor de ser TLS 1.2+, puedes leer esta publicación del blog <a href = "https://phabricator.miraheze.org/phame/post/view/4/potentially_breaking_security_changes/"> aquí </a>',
+		"ko"=>"네, 안전합니다. 우리는 TLS 1.2+가 되는 것에 대한 호의로 TLS 1.0과 TLS 1.1을 비활성화, 당신은 <a href='https://phabricator.miraheze.org/phame/post/view/4/potentially_breaking_security_changes/'>여기서</a> 블로그 포스트를 읽을 수 있다."
         ],
 	'financeqtext' => [
                 'en' => 'How do you finance?',
                 'es' => '¿Cómo se financia?',
+		"ko"=>"당신은 어떻게 자금을 댑니까?"
         ],
 	'financeatext' => [
                 'en' => 'We rely on donations to maintain the servers, please see our finances at <a href="https://meta.miraheze.org/wiki/Finance">Finance!</a>',
                 'es' => 'Confiamos en las donaciones para mantener los servidores, consulte nuestras finanzas en <a href="https://meta.miraheze.org/wiki/Finance"> Finance! </a>',
+		"ko"=>"우리는 서버를 유지하기 위한 도네이션에 의존하며, <a href='https://meta.miraheze.org/wiki/Finance'>재정!</a>에서 재정을 보세요."
         ],
 	'phabqtext' => [
                 'en' => 'Where can I report bugs/feature requests?',
                 'es' => '¿Dónde puedo reportar errores/solicitudes de características?',
+		"ko"=>"나는 어떻게 버그/기능 요청을 제보할 수 있습니까?"
         ],
 	'phabatext' => [
                 'en' => 'We use software called <a href="https://www.phacility.com/">Phabricator</a> located at <a href="https://phabricator.miraheze.org">phabricator.miraheze.org</a> to track bug reports or feature suggestions.',
                 'es' => 'Usamos el software llamado <a href="https://www.phacility.com/"> Phabricator </a> ubicado en <a href="https://phabricator.miraheze.org"> phabricator.miraheze.org < / a> para rastrear informes de errores o sugerencias de funciones.',
+		"ko"=>"우리는 버그 리포트와 기능 추천을 추적하기 위해 <a href='https://phabricator.miraheze.org'>phabricator.miraheze.org</a>에 위치한 <a href='https://www.phacility.com/'>파뿌리케이터</a>라 불리는 소프트웨어를 사용합니다."
         ],
 	'moretext' => [
                 'en' => 'View More',
                 'es' => 'Ver Mas',
+		"ko"=>"더 많이 보기"
         ],
 	'transparenttext' => [
                 'en' => 'Transparent and free!',
                 'es' => '¡Transparente y gratis!',
+		"ko"=>"투명하고 무료!"
         ],
 	'transparent2text' => [
                 'en' => 'We started in 2015 as a transparent non-profit wiki farm. Since then we have grown. We are transparent with our finance or financial information which you can view <a href="https://meta.miraheze.org/wiki/Finance">here!</a>',
                 'es' => 'Comenzamos en 2015 como una granja de wiki transparente sin fines de lucro. Desde entonces hemos crecido. Somos transparentes con nuestra información financiera o financiera que puede ver <a href="https://meta.miraheze.org/wiki/Finance"> aquí! </a>',
+		"ko"=>"우리는 2015년에 비영리 위키 농장과 같이 시작했습니다. 그때부터 우리는 성장했습니다. 우리는 당신이 <a href='https://meta.miraheze.org/wiki/Finance'>여기</a>에서 볼 수 있는 우리 재정과 함께 투명합니다."
         ],
 	'donatenowtext' => [
                 'en' => 'Donate now!',
                 'es' => 'Donar ahora!',
+		"ko"=>"지금 후원하기!"
         ],
 	'moneytext' => [
                 'en' => 'Amount of money',
                 'es' => 'Cantidad de dinero',
+		"ko"=>"돈의 양"
         ],
 	'paypaltext' => [
                 'en' => 'Donate with PayPal!',
                 'es' => 'Donar con PayPal!',
+		"ko"=>"페28과 함께 후원하기!"
         ],
 	'donatetext' => [
                 'en' => 'Donating to Miraheze has its benefits! You will be supporting our mission to provide a free community-centric wiki farm, that is secure and free of ads giving you peace of mind! We will use this funding to keep existing servers operational, and we will also buy some new servers to expand due to our growing user/wiki base.',
                 'es' => '¡Donar a Miraheze tiene sus beneficios! Estarás apoyando nuestra misión de proporcionar una granja de wiki gratuita centrada en la comunidad, que sea segura y sin anuncios, ¡lo que te dará tranquilidad! Usaremos esta financiación para mantener operativos los servidores existentes, y también compraremos algunos servidores nuevos para expandir debido a nuestra creciente base de usuarios / wiki.',
+		"ko"=>"Miraheze에 후원하는 것은 혜택이 있습니다! 당신은 우리가 안전하고 광고가 없어 마음의 평화를 주는 커뮤니티 중심의 위키 농장을 제공하는 무료 커뮤니티 중심의 위키 농장을 만드는 우리의 미션을 지원할 것입니다! 우리는 이 펀딩을 기존의 서버가 작동하는 것을 유지하기 위해 사용할 것이며, 우리는 또한 성장하는 사용자/위키 기초 때문에 새로운 서버를 살 것입니다."
         ],
 	'notetext' => [
                 'en' => 'Note that these are in US dollars, you will be able to convert it to your currency once on PayPal site.',
                 'es' => 'Tenga en cuenta que estos están en dólares estadounidenses, podrá convertirlos a su moneda una vez en el sitio de PayPal.',
+		"ko"=>"이것은 US 달러 안에 있다는 것을 언급하고, 당신은 한 번 페28 사이트에서 통화를 변환할 수 있습니다."
         ],
 	'dismisstext' => [
                 'en' => 'Dismiss',
                 'es' => 'Despedir',
+		"ko"=>"해고"
         ],
 	'toutext' => [
                 'en' => 'Terms of Use',
                 'es' => 'Términos de Uso',
+		"ko"=>"사용의 조건"
         ],
 	'pptext' => [
                 'en' => 'Privacy Policy',
                 'es' => 'Politica de Privacidad',
+		"ko"=>"사생활 정책"
         ],
 	'dtext' => [
                 'en' => 'Disclaimers',
                 'es' => 'Renuncias',
+		"ko"=>"권리 포기 각서"
         ],
 	'ctext' => [
                 'en' => 'Cookie Statement',
                 'es' => 'Declaración de Cookie',
+		"ko"=>"쿠키 성명"
         ],
 	'copytext' => [
                 'en' => 'Except where otherwise noted, the content of this site is licensed under a <a href="https://creativecommons.org/licenses/by-sa/4.0/">Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)</a>.',
                 'es' => 'Excepto donde se indique lo contrario, el contenido de este sitio se otorga bajo una <a href="https://creativecommons.org/licenses/by-sa/4.0/"> Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) </a>.',
+		"ko"=>"따로 주목되어 있는 곳을 빼고, 이 사이트의 콘텐츠는 <a href='https://creativecommons.org/licenses/by-sa/4.0/'>귀속-비슷하게 공유 4.0 국제 (CC BY-SA 4.0)</a> 아래에 라이선스되어 있습니다."
         ],
 ]
 ?>
