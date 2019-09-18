7c84e6eab24863373b8800bd9802ca025e231d06
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-09-18 19:05:45 +0100

    Activate Korean Translations (#14)
    Please double check Google translate

--- index.php
+++ index.php
@@ -172,6 +172,7 @@ if ( isset($_GET['lang']) ) {
                                 <option value="en">English</option>
                                 <option value="es">Español</option>
                                 <option value="fr">Français</option>
+                                <option value="ko">한국어</option>
                             </select></form>
                         </span>
                         <a class="navbar-item" href="https://github.com/miraheze/landing">
