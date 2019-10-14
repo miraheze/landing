f2e198aefd662af01eefdedc32d445939fe50c96
Giorgio68 <56552241+Giorgio68@users.noreply.github.com>
2019-10-14 17:56:43 +0200

    italian option value added

--- index.php
+++ index.php
@@ -173,6 +173,7 @@ if ( isset($_GET['lang']) ) {
                                 <option value="es">Español</option>
                                 <option value="fr">Français</option>
                                 <option value="ko">한국어</option>
+                                <option value="it">Italiano</option>
                             </select></form>
                         </span>
                         <a class="navbar-item" href="https://github.com/miraheze/landing">
