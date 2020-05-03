e66b33e9ef190964b6b23a43391c72b50859c062
Aobi Chan CL <eason066@gmail.com>
2020-05-03 22:46:21 +0800

    add zh-hant language

--- donate.php
+++ donate.php
@@ -85,9 +85,10 @@ require_once __DIR__ . '/translations.php';
                     <option value="en">English</option>
                     <option value="es">Español</option>
                     <option value="fr">Français</option>
-                    <option value="ko">한국어</option>
                     <option value="it">Italiano</option>
+                    <option value="ko">한국어</option>
                     <option value="pl">Polski</option>
+                    <option value="zh-hant">中文（繁體）</option>
                   </select>
                 </form>
               </div>
