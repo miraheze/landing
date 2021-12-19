493e13a79137ed3e397f5c42135a129cd30adf36
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-19 10:37:12 -0700

    Update getTranslations.php

--- getTranslations.php
+++ getTranslations.php
@@ -93,7 +93,7 @@ function getDefault() {
 }
 
 function getSiteNotice() {
-	$siteNotice = getTranslation( 'donate-sitenotice' );
+	$siteNotice = getTranslation( 'fundraising-sitenotice' );
 	$siteNotice = str_replace( '$1', '£12 ($16.35 USD)', $siteNotice );
 	$siteNotice = str_replace( '$2', 'help-miraheze-stay-online-in-2022', $siteNotice );
 
