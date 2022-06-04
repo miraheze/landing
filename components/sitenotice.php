d79b481fce7569afa5076a17ec1f4dbec28109b6
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-03 18:57:41 -0600

    Create sitenotice.php

---
+++ components/sitenotice.php
@@ -0,0 +1,24 @@
+<?php if ( getSiteNotice() ): ?>
+	<section class="sitenotice">
+		<div class="card">
+			<div class="row g-0">
+				<div class="col-md-2 d-flex justify-content-center align-items-center">
+					<img src="https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg" class="img-fluid rounded-start" alt="Miraheze Logo">
+				</div>
+				<div class="col-md-8">
+					<div class="card-body">
+						<h5 class="card-title">
+							<?php echo getTranslation( 'fundraiser-sitenotice-title' ); ?>
+						</h5>
+						<p class="card-text">
+							<?php echo getTranslation( 'fundraising-sitenotice' ); ?>
+						</p>
+						<a href="https://miraheze.wiki/Fundraiser" class="btn btn-primary">
+							<?php echo getTranslation( 'fundraising-sitenotice-button-text' ); ?>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<?php endif; ?>
