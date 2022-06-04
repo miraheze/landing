d42296282b05e6eed51aaefff17c99195438c761
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-03 20:11:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -107,5 +107,21 @@
 		</section>
 
 		<?php include_once __DIR__ . '/components/footer.php'; ?>
+		<script>
+			var _paq = window._paq = window._paq || [];
+			_paq.push(['disableCookies']);
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function() {
+				var u = 'https://matomo.miraheze.org/';
+				_paq.push(['setTrackerUrl', u+'matomo.php']);
+				_paq.push(['setDocumentTitle', 'Landing - Home']);
+				_paq.push(['setSiteId', 4657]);
+				_paq.push(['addTracker', u + 'matomo.php', 8590]);
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+				g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+			})();
+		</script>
+		<noscript><p><img src="https://matomo.miraheze.org/matomo.php?idsite=4657&amp;rec=1&amp;action_name=Landing_Home" style="border:0;" alt="" /></p></noscript>
 	</body>
 </html>
