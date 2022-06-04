f561c38282bcc28e1f8f7f8d7650b54bfc90ec72
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-03 19:01:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,7 @@ require_once __DIR__ . '/getTranslations.php';
 
 <!DOCTYPE html>
 <html lang="<?php echo getLanguageCode(); ?>">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-		<meta name="description" content="Miraheze is an open source project that offers free MediaWiki hosting, for everyone. Request your free wiki today!" />
-		<meta name="revisit-after" content="2 days" />
-		<meta name="keywords" content="miraheze, free, wiki hosting, mediawiki, mediawiki hosting, open source, hosting" />
-		<title><?php echo getTranslation( 'header-logo-text' ); ?></title>
-		<link rel="icon" type="image/x-icon" href="https://meta.miraheze.org/favicon.ico" />
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
-		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-		<link href="css/styles.css" rel="stylesheet" />
-	</head>
+	<?php include_once __DIR__ . '/components/head.php'; ?>
 	<body>
 		<?php
 			include_once __DIR__ . '/components/navigation.php';
@@ -184,87 +173,7 @@ require_once __DIR__ . '/getTranslations.php';
 				</div>
 			</div>
 		</section>
-		<footer id="footer" class="footer bg-dark">
-			<div class="container">
-				<small>
-					<p style="text-align-last: center;">
-						<?php echo getTranslation( 'footer-desc' ); ?>
-					</p>
-				</small>
-				<div class="row">
-					<div class="col-lg-6 h-100 text-center text-lg-start my-auto">
-						<ul class="list-inline mb-2">
-							<li class="list-inline-item">
-								<a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Content_Policy">
-									<?php echo getTranslation( 'footer-content-policy' ); ?>
-								</a>
-							</li>
-							<li class="list-inline-item">⋅</li>
-							<li class="list-inline-item">
-								<a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Terms_of_Use">
-									<?php echo getTranslation( 'footer-terms' ); ?>
-								</a>
-							</li>
-							<li class="list-inline-item">⋅</li>
-							<li class="list-inline-item">
-								<a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Privacy_Policy">
-									<?php echo getTranslation( 'footer-privacy-policy' ); ?>
-								</a>
-							</li>
-							<li class="list-inline-item">⋅</li>
-							<li class="list-inline-item">
-								<a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Meta:General_disclaimer">
-									<?php echo getTranslation( 'footer-disclaimers' ); ?>
-								</a>
-							</li>
-							<li class="list-inline-item">⋅</li>
-							<li class="list-inline-item">
-								<a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Privacy_Policy#4._Cookies">
-									<?php echo getTranslation( 'footer-cookie' ); ?>
-								</a>
-							</li>
-						</ul>
-						<a class="small mb-4 mb-lg-0" href="https://translatewiki.net/wiki/Special:Translate?group=mwgithub-mirahezelanding">
-							<?php echo getTranslation( 'footer-translate' ); ?>
-						</a>
-					</div>
-					<div class="col-lg-6 h-100 text-center text-lg-end my-auto">
-						<ul class="list-inline mb-0">
-							<li class="list-inline-item me-4">
-								<a href="https://www.facebook.com/miraheze"><i class="bi-facebook fs-3"></i></a>
-							</li>
-							<li class="list-inline-item me-4">
-								<a href="https://twitter.com/miraheze"><i class="bi-twitter fs-3"></i></a>
-							</li>
-							<li class="list-inline-item">
-								<a href="https://miraheze.org/discord"><i class="bi-discord fs-3"></i></a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<div id="scroll-to-top">
-			<a class="text-light" href="#">
-				<i class="bi bi-chevron-up"></i>
-			</a>
-		</div>
 
-		<script>
-			scrollbutton = document.getElementById( 'scroll-to-top' );
-			window.onscroll = function() {
-				scrollFunction()
-			};
-
-			function scrollFunction() {
-				if ( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
-					scrollbutton.style.display = 'block';
-				} else {
-					scrollbutton.style.display = 'none';
-				}
-			}
-		</script>
-
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+		<?php include_once __DIR__ . '/components/footer.php'; ?>
 	</body>
 </html>
