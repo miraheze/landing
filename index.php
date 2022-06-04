ee0979761d0b55df049405bc2d8f243245b9dd50
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-03 18:44:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -43,28 +43,30 @@ require_once __DIR__ . '/getTranslations.php';
 				</div>
 			</div>
 		</nav>
-		<section class="sitenotice">
-			<div class="card">
-				<div class="row g-0">
-					<div class="col-md-2 d-flex justify-content-center align-items-center">
-						<img src="https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg" class="img-fluid rounded-start" alt="Miraheze Logo">
-					</div>
-					<div class="col-md-8">
-						<div class="card-body">
-							<h5 class="card-title">
-								<?php echo getTranslation( 'fundraiser-sitenotice-title' ); ?>
-							</h5>
-							<p class="card-text">
-								<?php echo getTranslation( 'fundraising-sitenotice' ); ?>
-							</p>
-							<a href="https://miraheze.wiki/Fundraiser" class="btn btn-primary">
-								<?php echo getTranslation( 'fundraising-sitenotice-button-text' ); ?>
-							</a>
+		<?php if ( getSiteNotice() ): ?>
+			<section class="sitenotice">
+				<div class="card">
+					<div class="row g-0">
+						<div class="col-md-2 d-flex justify-content-center align-items-center">
+							<img src="https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg" class="img-fluid rounded-start" alt="Miraheze Logo">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body">
+								<h5 class="card-title">
+									<?php echo getTranslation( 'fundraiser-sitenotice-title' ); ?>
+								</h5>
+								<p class="card-text">
+									<?php echo getTranslation( 'fundraising-sitenotice' ); ?>
+								</p>
+								<a href="https://miraheze.wiki/Fundraiser" class="btn btn-primary">
+									<?php echo getTranslation( 'fundraising-sitenotice-button-text' ); ?>
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		<?php endif; ?>
 		<header class="masthead">
 			<div class="container position-relative">
 				<div class="row justify-content-center">
