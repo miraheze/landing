cb90a7f2b6fef7670cd4623ce00a08e28290c24a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-05 18:51:53 -0600

    Minor improvements (#85)

--- donate.php
+++ donate.php
@@ -3,38 +3,27 @@
 <!DOCTYPE html>
 <html lang="<?php echo getLanguageCode(); ?>">
 	<?php include_once __DIR__ . '/components/head.php'; ?>
-	<body>
+	<body class="bg-white">
 		<?php
 			include_once __DIR__ . '/components/navigation.php';
 			include_once __DIR__ . '/components/sitenotice.php';
 			include_once __DIR__ . '/components/header.php';
 		?>
-		<section id="donate" class="section-donate bg-light" style="padding: 5rem 0;">
+		<section id="donate" class="section-donate my-5">
 			<div class="container">
 				<h2 class="page-section-heading text-center text-uppercase">
 					<?php echo getTranslation( 'donate-heading' ); ?>
 				</h2>
-				<div class="row">
-					<div class="col-lg-8 mx-auto">
-						<form name="donate" id="donateForm" novalidate="novalidate">
-							<div class="control-group">
-								<div class="form-group floating-label-form-group controls mb-0 pb-2">
-									<input class="form-control" id="amountInput" type="number" placeholder="<?php echo getTranslation( 'donate-amount' ); ?>" oninput="_amountInput(this)">
-								</div>
-							</div>
-							<br />
-							<div class="form-group">
-								<a id="donationHref" class="btn btn-primary btn-xl" href="https://paypal.me/mirahezelimited">
-									<?php echo getTranslation( 'donate-heading' ); ?>
-								</a>
-								<br />
-								<small>
-									<?php echo getTranslation( 'donate-note' ); ?>
-								</small>
-							</div>
-						</form>
+				<form name="donate" id="donateForm">
+					<div class="form-control-lg mb-0 pb-2 col-lg-8 mx-auto">
+						<input class="form-control" id="amountInput" type="number" placeholder="<?php echo getTranslation( 'donate-amount' ); ?>" oninput="_amountInput(this)">
+
+						<a id="donationHref" class="btn btn-primary mt-1" href="https://paypal.me/mirahezelimited">
+							<?php echo getTranslation( 'donate-heading' ); ?>
+						</a>
+						<p class="col-form-label-sm py-0"><?php echo getTranslation( 'donate-note' ); ?></p>
 					</div>
-				</div>
+				</form>
 			</div>
 		</section>
 
@@ -65,6 +54,6 @@
 				a.setAttribute( 'href', 'https://paypal.me/mirahezelimited' + value );
 			}
 		</script>
-		<noscript><p><img src="https://matomo.miraheze.org/matomo.php?idsite=4657&amp;rec=1&amp;action_name=Landing_Donate" style="border:0;" alt="" /></p></noscript>
+		<noscript><p><img src="https://matomo.miraheze.org/matomo.php?idsite=4657&amp;rec=1&amp;action_name=Landing_Donate" style="border: 0;" alt="" /></p></noscript>
 	</body>
 </html>
