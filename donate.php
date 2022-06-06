290ef2c9fca414f08d7f4bde1c60a88474878cb4
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-05 18:16:26 -0600

    Update donate.php

--- donate.php
+++ donate.php
@@ -16,10 +16,11 @@
 				</h2>
 				<form class="pb-5 col-lg-8 mx-auto" id="donateForm" name="donate">
 					<div class="input-group">
+						<span class="input-group-text">£</span>
 						<input class="form-control form-control-lg" id="amountInput" type="number" placeholder="<?php echo getTranslation( 'donate-amount' ); ?>" oninput="_amountInput(this)" />
-							<a id="donationHref" class="btn btn-primary btn-lg" href="https://paypal.me/mirahezelimited">
-								<?php echo getTranslation( 'donate-heading' ); ?>
-							</a>
+						<a id="donationHref" class="btn btn-primary btn-lg" href="https://paypal.me/mirahezelimited">
+							<?php echo getTranslation( 'donate-heading' ); ?>
+						</a>
 					</div>
 					<p class="col-form-label-sm py-0"><?php echo getTranslation( 'donate-note' ); ?></p>
 				</form>
