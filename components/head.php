269cdd5d427eb9a9b1d771b6344443d4fc206fd8
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-10-22 14:33:24 -0600

    Add some SEO improvements

--- components/head.php
+++ components/head.php
@@ -1,18 +1,34 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-	<meta name="description" content="Miraheze is an open source project that offers free MediaWiki hosting, for everyone. Request your free wiki today!" />
+	<meta name="description" content="Miraheze is a community-centric, ad free, 100% open-source, and free wiki hosting platform powered by MediaWiki, the software that powers Wikipedia." />
 	<meta name="revisit-after" content="2 days" />
-	<meta name="keywords" content="miraheze, free, ad-free, ad free, wiki hosting, mediawiki, mediawiki hosting, open source, hosting, donate, not-for-profit" />
+	<meta name="keywords" content="miraheze, free, ad-free, ad free, wiki hosting, wiki farm, wiki farms, sign up, mediawiki, mediawiki hosting, open source, hosting, donate, not-for-profit, not for profit, notforprofit, non-profit, non profit, nonprofit" />
 	<title>
 		<?php
 			if ( $_SERVER['SCRIPT_NAME'] === '/donate.php' ) {
-				echo getTranslation( 'header-donate-logo-text' );
+				echo 'Miraheze | Donate';
 			} else {
-				echo getTranslation( 'header-logo-text' );
+				echo 'Miraheze | Free MediaWiki Hosting!';
 			}
 		?>
 	</title>
+	<?php
+		$hostname = 'miraheze.org';
+		if ( $_SERVER['SCRIPT_NAME'] === '/donate.php' ) {
+			$hostname = 'donate.miraheze.org';
+		}
+
+		$i18nDirectory = array_diff( scandir( 'i18n' ), [ '.', '..', 'qqq.json' ] );
+		$languageCodes = str_replace( '.json', '', $i18nDirectory );
+
+		foreach ( $languageCodes as $languageCode ) {
+			echo "<link rel=\"alternate\" hreflang=\"{$languageCode}\" href=\"https://{$hostname}?lang={$languageCode}\" />";
+		}
+
+		echo "<link rel=\"alternate\" href=\"https://{$hostname}\" hreflang=\"x-default\" />";
+		echo "<link rel=\"canonical\" href=\"https://{$hostname}\" />";
+	?>
 	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
