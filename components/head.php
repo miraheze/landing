d0851c8ea4ebcdd8c1a3bb826ab6902e6dca1e94
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-11-06 11:56:34 -0700

    Add og:image meta tags (#92)

--- components/head.php
+++ components/head.php
@@ -6,32 +6,40 @@
 	<meta name="description" content="Miraheze is an ad-free and community-centric wiki hosting platform where anyone can create a free MediaWiki-powered wiki. Request your free wiki today!" />
 	<meta name="twitter:description" content="Miraheze is an ad-free and community-centric wiki hosting platform where anyone can create a free MediaWiki-powered wiki. Request your free wiki today!" />
 	<meta property="og:description" content="Miraheze is an ad-free and community-centric wiki hosting platform where anyone can create a free MediaWiki-powered wiki. Request your free wiki today!" />
+	<meta property="og:image" content="/static/Miraheze-Logo-1200px.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="1200" />
+	<meta property="og:image" content="/static/Miraheze-Logo-800px.png" />
+	<meta property="og:image:width" content="800" />
+	<meta property="og:image:height" content="800" />
+	<meta property="og:image" content="/static/Miraheze-Logo-640px.png" />
+	<meta property="og:image:width" content="640" />
+	<meta property="og:image:height" content="640" />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Miraheze" />
 	<meta property="og:site_name" content="Miraheze" />
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:title" content="Miraheze" />
 	<meta name="twitter:site" content="@miraheze" />
-	<title>
-		<?php
-			if ( $_SERVER['SCRIPT_NAME'] === '/donate.php' ) {
-				echo 'Miraheze | Donate';
-			} else {
-				echo 'Miraheze';
-			}
-		?>
-	</title>
+	<meta name="twitter:image" content="https://miraheze.org/static/Miraheze-Logo-1200px.png" />
+	<title><?php
+		if ( $_SERVER['SCRIPT_NAME'] === '/donate.php' ) {
+			echo 'Miraheze | Donate';
+		} else {
+			echo 'Miraheze';
+		}
+	?></title>
 	<?php
 		$hostname = 'miraheze.org';
 		if ( $_SERVER['SCRIPT_NAME'] === '/donate.php' ) {
 			$hostname = 'donate.miraheze.org';
 		}
 
-		echo "<link rel=\"canonical\" href=\"https://{$hostname}\" />";
-		echo "<meta property=\"og:url\" content=\"https://{$hostname}\" />";
+		echo "<link rel=\"canonical\" href=\"https://{$hostname}\" />\n";
+		echo "<meta property=\"og:url\" content=\"https://{$hostname}\" />\n";
 	?>
-	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+	<link rel="apple-touch-icon" href="/static/apple-touch-icon.png" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
 	<link href="css/styles.css" rel="stylesheet" />
