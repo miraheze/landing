92163f6c1ecbdeb7121caf3c5c51f9ea43b8d3e7
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-11-10 00:46:35 -0700

    Use "Miraheze Donate" for headline on donate page (#98)

--- components/head.php
+++ components/head.php
@@ -57,5 +57,12 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
 	<link href="/css/styles.css" rel="stylesheet" />
-	<script type="application/ld+json">[{"@context":"https:\/\/schema.org","@type":"WebPage","headline":"Miraheze Home Page","url":"/","thumbnailUrl":"","dateCreated":"","creator":[],"keywords":[]}]</script>
+	<?php
+		$headline = 'Miraheze';
+		if ( $_SERVER['SCRIPT_NAME'] === '/donate.php' ) {
+			$headline = 'Miraheze Donate';
+		}
+
+		echo "\t" . '<script type="application/ld+json">[{"@context":"https:\/\/schema.org","@type":"WebPage","headline":"' . $headline . '","url":"/","thumbnailUrl":"","dateCreated":"","creator":[],"keywords":[]}]</script>' . "\n";
+	?>
 </head>
