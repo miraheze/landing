f7ea5136c9be07577a8580d975b1bb8c03568e5b
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-11-24 13:56:27 -0700

    Update head.php

--- components/head.php
+++ components/head.php
@@ -57,7 +57,7 @@
 	<link rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-	<link href="/css/styles.css" rel="stylesheet" />
+	<link href="/css/styles.min.css" rel="stylesheet" />
 	<?php
 		$headline = 'Miraheze';
 		if ( $_SERVER['SCRIPT_NAME'] === '/donate.php' ) {
