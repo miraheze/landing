e059b8722c7e94c9f36bebb2aed04f589c68fe0a
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2024-01-30 22:12:33 -0600

    Add Matomo

--- index.php
+++ index.php
@@ -49,6 +49,24 @@
 
     <!-- Template Stylesheet -->
     <link href="https://miraheze.org/css/style.css" rel="stylesheet">
+
+    <!-- Matomo analytics -->
+    <script>
+	    var _paq = window._paq = window._paq || [];
+		_paq.push(['disableCookies']);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u = 'https://matomo.miraheze.org/';
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setDocumentTitle', 'Landing - Home']);
+			_paq.push(['setSiteId', 9838]);
+			_paq.push(['addTracker', u + 'matomo.php', 1]);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<noscript><p><img src="https://matomo.miraheze.org/matomo.php?idsite=9838&amp;rec=1&amp;action_name=Landing_Home" style="border:0;" alt="Matomo" /></p></noscript>
 </head>
 
 <body>
