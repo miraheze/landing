f4641808806bbda7db66d14189f5abb63e36d08e
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-31 08:24:52 -0700

    Merge from WikiTide (#131)
    * Merge from WikiTide (#130)
    
    * Merge from WikiTide
    
    * Delete components
    
    * Change to point to CSP entries
    
    * Fix Miraheze Foundation -> WikiTide Foundation
    
    * Fix links
    
    * Add Matomo
    
    * Add language dropdown
    
    ---------
    
    Co-authored-by: Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
    
    * Correct button text
    
    * Fix owlCarousel error
    
    * Fix broken i18n
    
    * Fix i18n
    
    * Convert to webp and fix JS
    
    ---------
    
    Co-authored-by: Agent_Isai <43097272+AgentIsai@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function ($) {
+    "use strict";
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });    
+})(jQuery);
+
