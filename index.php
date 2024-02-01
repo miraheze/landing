80a77b629020da8a31836907789db6e9c6765bc0
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-01 10:47:22 -0700

    Re-add bootstrap JS

--- index.php
+++ index.php
@@ -438,5 +438,8 @@
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-secondary btn-lg-square back-to-top" title="Back to top"><i class="fa fa-arrow-up"></i></a>
     </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
