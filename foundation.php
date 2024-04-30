1a21a4b290ffe84f949dd4feec81bbf5c53d9047
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-04-30 11:15:02 -0600

    Use board email

--- foundation.php
+++ foundation.php
@@ -174,12 +174,7 @@
                 <div class="row g-5">
                     <div class="col-md-6 col-lg-3">
                         <p class="section-title text-accent h5 mb-4">Contact us<span></span></p>
-                        <p><i class="fa fa-envelope me-3"></i><span class="text-accent">sre [at] wikitide.org</span></p>
-                        <p><i class="fa fa-globe me-3"></i><a class="text-accent"
-                                href="https://meta.miraheze.org/wiki/Special:MyLanguage/Help_center"
-                                title="On-wiki">On-wiki</a></p>
-                        <p><i class="fa fa-flag-checkered me-3"></i><a class="text-accent"
-                                href="https://issue-tracker.miraheze.org" title="Issue tracker">Issue tracker</a></p>
+                        <p><i class="fa fa-envelope me-3"></i><span class="text-accent">board [at] wikitide.org</span
 
                         <div class="d-flex pt-2">
                             <a rel="me" class="btn btn-outline-light btn-social"
